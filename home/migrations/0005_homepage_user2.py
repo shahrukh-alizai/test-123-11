c9e726f861065391a1fada28d2be93aeb63083ad
Shah Rukh Khan <shah@crowdbotics.com>
2020-07-29 11:20:34 +0000

    Update models

--- home/migrations/0005_homepage_user2.py
+++ home/migrations/0005_homepage_user2.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0004_homepage_rwat'),
+        ("home", "0004_homepage_rwat"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='homepage',
-            name='user2',
+            model_name="homepage",
+            name="user2",
             field=models.BigIntegerField(blank=True, null=True),
         ),
     ]
