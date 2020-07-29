a337b60d9691c31c7973ea2fd82bdcb1e5497b6e
Shah Rukh Khan <shah@crowdbotics.com>
2020-07-29 11:20:08 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -25,6 +25,7 @@ class CustomText(models.Model):
 class HomePage(models.Model):
     body = models.TextField()
     rwat = models.BigIntegerField(null=True, blank=True,)
+    user2 = models.BigIntegerField(null=True, blank=True,)
 
     @property
     def api(self):
