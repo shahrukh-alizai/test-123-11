25c9cc5d1f778e9884a2f30bc9ff9da059512f7e
Shah Rukh Khan <shah@crowdbotics.com>
2020-07-29 11:21:38 +0000

    Update models

--- home/migrations/0007_remove_homepage_tests.py
+++ home/migrations/0007_remove_homepage_tests.py
@@ -6,12 +6,9 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0006_auto_20200729_1120'),
+        ("home", "0006_auto_20200729_1120"),
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='homepage',
-            name='tests',
-        ),
+        migrations.RemoveField(model_name="homepage", name="tests",),
     ]
